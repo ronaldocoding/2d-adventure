bda1e788935ee3a67550636c5d31578234d80d66
ronaldocosta <ronaldocosta.developer@gmail.com>
2022-05-06 20:02:29 -0400

    Add hero's basic movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,10 +11,13 @@ public class Player : MonoBehaviour
     public bool doubleJump;
     private Rigidbody2D rig;
 
+    private Animator anim;
+    
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -28,6 +31,16 @@ public class Player : MonoBehaviour
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * Speed;
+        if(Input.GetAxis("Horizontal") > 0f) {
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+        else if(Input.GetAxis("Horizontal") < 0f) {
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        } else {
+             anim.SetBool("walk", false);
+        }
     }
 
     void Jump() {
@@ -36,6 +49,7 @@ public class Player : MonoBehaviour
             if(!isJumping) {
                 rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
                 doubleJump = true;
+                anim.SetBool("jump", true);
             } else {
                 if(doubleJump) {
                     rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
@@ -49,6 +63,7 @@ public class Player : MonoBehaviour
     {
         if(collision.gameObject.layer == 8) {
             isJumping = false;
+            anim.SetBool("jump", false);
         }
     }
 
