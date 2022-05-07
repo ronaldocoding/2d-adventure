4e7aa0d7c2fe5057edab7742807bd22612743da9
ronaldocosta <ronaldocosta.developer@gmail.com>
2022-05-07 17:21:15 -0400

    Add game over logic and panel

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class GameController : MonoBehaviour
     public Text scoreText;
 
     public static GameController instance;
+    public GameObject gameOver;
 
     // Start is called before the first frame update
     void Start()
@@ -25,5 +27,15 @@ public class GameController : MonoBehaviour
         } else {
             scoreText.text = totalScore.ToString();
         }
+    }
+
+    public void ShowGameOver()
+    {
+        gameOver.SetActive(true);
+    }
+
+    public void RestartGame(string lvl_name)
+    {
+        SceneManager.LoadScene(lvl_name);
     }
 }
