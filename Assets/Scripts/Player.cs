1791bc3076832762efcac30c5a25dbe7f7fa4d16
ronaldocosta <ronaldocosta.developer@gmail.com>
2022-05-08 02:35:49 -0400

    Fix minor bugs and improvements

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -30,13 +30,14 @@ public class Player : MonoBehaviour
 
     void Move() 
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += movement * Time.deltaTime * Speed;
-        if(Input.GetAxis("Horizontal") > 0f) {
+        float movement = Input.GetAxis("Horizontal");
+        rig.velocity = new Vector2(movement * Speed, rig.velocity.y);
+
+        if(movement > 0f) {
             anim.SetBool("walk", true);
             transform.eulerAngles = new Vector3(0f, 0f, 0f);
         }
-        else if(Input.GetAxis("Horizontal") < 0f) {
+        else if(movement < 0f) {
             anim.SetBool("walk", true);
             transform.eulerAngles = new Vector3(0f, 180f, 0f);
         } else {
