39c70f410063cb4b4165ac3ee0faba007e41d04d
ronaldocosta <ronaldocosta.developer@gmail.com>
2022-05-07 22:46:06 -0400

    Create frog enemy and add its logic

---
+++ Assets/Scripts/Frog.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frog : MonoBehaviour
+{
+
+    private Rigidbody2D rig;
+    private Animator anim;
+    private bool colliding;
+    public float speed;
+    public Transform righColl;
+    public Transform leftColl;
+    public Transform headPoint;
+    public LayerMask layer;
+    public CircleCollider2D circleColl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rig.velocity = new Vector2(speed, rig.velocity.y);
+        colliding = Physics2D.Linecast(
+            righColl.position,
+            leftColl.position,
+            layer
+        );
+
+        if(colliding)
+        {
+            transform.localScale = new Vector2(
+                transform.localScale.x * -1f,
+                transform.localScale.y
+            );
+            speed *= -1f;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            float height = collision.contacts[0].point.y - headPoint.position.y;
+
+            if(height > 0)
+            {
+                collision.gameObject.GetComponent<Rigidbody2D>()
+                .AddForce(Vector2.up * 5, ForceMode2D.Impulse);
+                speed = 0;
+                anim.SetTrigger("death");
+                circleColl.enabled = false;
+                rig.bodyType = RigidbodyType2D.Kinematic;
+                Destroy(gameObject, 0.33f);
+            }
+        }
+    }
+}
